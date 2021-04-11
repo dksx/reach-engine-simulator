794ed132004c3d1c007514f7062c65f8455a85b0
dksx <37441849+dksx@users.noreply.github.com>
2021-04-11 22:05:03 +0200

    Redesign fetch execution and add in memory db

--- app/reach_engine.py
+++ app/reach_engine.py
@@ -3,15 +3,18 @@ import sys
 import uuid
 import boto3.session as s3session
 from botocore.exceptions import ClientError
-from flask import Flask, request
+from flask import Flask, request, g
 from uwsgidecorators import *
 import datetime as dt
+from db import fetch_data, insert_data, delete_data, update_data, db_lookup
+import sqlite3
 import csv
 import json
 import random
 
 ACCESS_KEY_AWS = ''
 SECRET_KEY_AWS = ''
+DATABASE = 'reach_engine:cachedb?mode=memory&cache=shared'
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
 hashmap_ = dict()
@@ -20,10 +23,22 @@ with open('dcl.csv', 'r') as csvfile:
     for i in data:
         hashmap_[i[1]] = i[4]
 
-t_abort = dict(timeout=90)
-
 app = Flask(__name__)
 
+
+def connect_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def initialize_database():
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS executions")
+    cursor.execute("CREATE TABLE executions(execution_id TEXT PRIMARY KEY, time REAL)")
+    cursor.execute("INSERT INTO executions(execution_id, time) VALUES(?,?)", ("timeout", 90,))
+    connection.commit()
 
 @cron(-10, -1, -1, -1, -1)
 def run_periodic_task(num):
@@ -125,18 +140,24 @@ def execution_status(execution_id):
     if request.method != 'GET':
         return '', 511
     try:
-        if execution_id in t_abort:
-            time_ = t_abort[execution_id]
-            if dt.datetime.utcnow() >= time_:
+        connection = connect_db()
+        cursor = connection.cursor()
+        data = fetch_data(cursor, execution_id)
+        if(data):
+            completion = data[0][-1]
+            if dt.datetime.timestamp(dt.datetime.utcnow()) >= completion:
                 response = dict(Id=execution_id, Status="COMPLETED", ErrorMessage="", PercentComplete="100")
-                del t_abort[execution_id]
+                delete_data(connection, cursor, execution_id)
                 return response, 200
             else:
                 response = dict(Id=execution_id, Status="EXECUTING", ErrorMessage="", PercentComplete="40")
-                return response, 200
-        t_abort[execution_id] = dt.datetime.utcnow() + dt.timedelta(seconds=t_abort['timeout'])
-        response = dict(Id=execution_id, Status="EXECUTING", ErrorMessage="", PercentComplete="20")
-        return response, 200
+                return response, 200 
+        else:
+            timeout = fetch_data(cursor, "timeout")[0][-1]
+            completion = dt.datetime.timestamp(dt.datetime.utcnow() + dt.timedelta(seconds=int(timeout)))
+            insert_data(connection, cursor, (execution_id, completion))
+            response = dict(Id=execution_id, Status="EXECUTING", ErrorMessage="", PercentComplete="20")
+            return response, 200
     except Exception as e:
         print(f'Exception - {e}')
         return dict(response=f'Error during execution - {e}'), 500
@@ -155,16 +176,56 @@ def set_timeout(seconds):
         return '', 511
     try:
         delay = int(seconds)
-        t_abort['timeout'] = delay
-        print(t_abort['timeout'])
-        return t_abort['timeout'], 200
+        connection = connect_db()
+        cursor = connection.cursor()
+        update_data(connection, cursor, (delay, "timeout"))
+        response = dict(response=f"Execution completion timeframe - {seconds}s")
+        return response, 200
     except Exception as e:
-        return f"{e}", 400
+        print(f"{e}")
+        return f"{e}", 500
+
+@app.route("/reachengine/db-lookup", methods=["GET"])
+def table_lookup():
+    if request.method != 'GET':
+        return '', 511
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        data = db_lookup(cursor)
+        response = dict(response=data)
+        return response, 200
+    except Exception as e:
+        print(f"{e}")
+        return f"{e}", 500
+
+@app.route("/reachengine/init", methods=["PUT"])
+def db_init():
+    if request.method != 'PUT':
+        return '', 511
+    try:
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS executions")
+        cursor.execute("CREATE TABLE executions(execution_id TEXT PRIMARY KEY, time REAL)")
+        cursor.execute("INSERT INTO executions(execution_id, time) VALUES(?,?)", ("timeout", 90,))
+        connection.commit()
+        return "", 200
+    except Exception as e:
+        print(f"{e}")
+        return f"{e}", 500
 
 @app.route('/<path:path>', methods=HTTP_METHODS)
 def fallback(path=None):
     return '', 511
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 #if __name__ == "__main__":
 #    app.run(debug=True, host='0.0.0.0', port=8080)
+with app.app_context():
+    initialize_database()
